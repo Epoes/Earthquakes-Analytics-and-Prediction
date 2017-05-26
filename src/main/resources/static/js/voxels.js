9e440667411e9434930958ed03265aa65978f93b
Epoes <leonardo.iandiorio@gmail.com>
2017-05-26 19:28:07 +0200

    work on the UI

--- src/main/resources/static/js/voxels.js
+++ src/main/resources/static/js/voxels.js
@@ -17,11 +17,11 @@ var viewer2= new Cesium.Viewer('cesiumContainer', {
 
     selectorIndicator : false,
     //Hide the base layer picker
-    // baseLayerPicker : false,
+    baseLayerPicker : false,
     //Use OpenStreetMaps
     imageryProvider : new Cesium.MapboxImageryProvider({
         url: 'https://api.mapbox.com/v4/',
-        mapId: 'mapbox.streets',
+        mapId: 'mapbox.streets-basic',
     }),
     // imageryProvider : new Cesium.ArcGisMapServerImageryProvider({
     //     url : 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
@@ -39,7 +39,7 @@ var viewer2= new Cesium.Viewer('cesiumContainer', {
 
     navigationInstructionInitiallyVisible: false,
 
-    // creditContainer: 'credit'
+    creditContainer: 'credit-container'
 
 });
 
@@ -50,14 +50,35 @@ scene.debugShowFramesPerSecond = true;
 
 var elevations;
 var epicentres;
+var stationMagnitudes;
+var stationMagnitudes2;
+var arrivals;
 
 $(document).ready(function () {
     doRequest(stdRequest);
-})
+    htmlbodyHeightUpdate()
+    $( window ).resize(function() {
+        htmlbodyHeightUpdate()
+    });
+    $( window ).scroll(function() {
+        height2 = $('.main').height()
+        htmlbodyHeightUpdate()
+    });
+});
+
+$("#generate-italy").click(function() {
+    doItalyRequest()
+});
+
 var unit = 911.3862583;
 var unitFake = 1000.4257813;
 var maxDepth = 35000;
-var geometry = new Cesium.BoxGeometry.fromDimensions({
+var geometryElevation = new Cesium.BoxGeometry.fromDimensions({
+    vertexFormat : Cesium.VertexFormat.POSITION_ONLY,
+    dimensions : new Cesium.Cartesian3(unit, unit, unit)
+});
+
+var geometryStation = new Cesium.BoxGeometry.fromDimensions({
     vertexFormat : Cesium.VertexFormat.POSITION_ONLY,
     dimensions : new Cesium.Cartesian3(unit, unit, unit)
 });
@@ -70,7 +91,7 @@ function drawItaly(data){
         var height = convertHeight(elevation.elevation);
 
         primitivesArray.push(new Cesium.GeometryInstance({
-                            geometry : geometry,
+                            geometry : geometryElevation,
                             modelMatrix : Cesium.Matrix4.multiplyByTranslation(
                                 Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(elevation.longitude, elevation.latitude)),
                                 new Cesium.Cartesian3(0.0, 0.0, height + maxDepth), new Cesium.Matrix4()),
@@ -106,7 +127,7 @@ function drawSide(data, primitivesArray, length, geometry){
             var elevation = data[i];
             var height = convertHeight(elevation.elevation);
             primitivesArray.push(new Cesium.GeometryInstance({
-                    geometry: geometry,
+                    geometry: geometryElevation,
                     modelMatrix: Cesium.Matrix4.multiplyByTranslation(
                         Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(elevation.longitude, elevation.latitude)),
                         new Cesium.Cartesian3(0.0, 0.0, j + height - unit), new Cesium.Matrix4()),
@@ -182,10 +203,10 @@ var centerBoundings = [43.961401, 14.452927, 42.424652, 10.050827];
 var italyBoundings = [48.00, 35.00, 19.00, 5.00];
 
 var stdRequest = {
-    count : 1,
+    count : 100,
     endTime : new Date(),
     startTime : new Date("1985.01.01"),
-    minMag : 3,
+    minMag : 5,
     maxMag : 9,
     minDepth : 0,
     maxDepth : 20000,
@@ -201,17 +222,19 @@ var stdRequest = {
 
 // stdRequest.startTime.setDate(stdRequest.startTime.getDate() - 100000000);
 
-function doRequest(request){
-    // $.ajax({
-    //     // url: "http://" + window.location.host + "/api/elevations/query?minele=-4200",
-    //     url: "http://" + window.location.host + "/api/elevations/query",
-    //     type: "GET",
-    //     success: function (data, textStatus, jqXHR) {
-    //         elevations = data;
-    //         drawItaly(elevations);
-    //     }
-    // });
+function doItalyRequest(){
+    $.ajax({
+        // url: "http://" + window.location.host + "/api/elevations/query?minele=-4200",
+        url: "http://" + window.location.host + "/api/elevations/query",
+        type: "GET",
+        success: function (data, textStatus, jqXHR) {
+            elevations = data;
+            drawItaly(elevations);
+        }
+    });
+}
 
+function doRequest(request){
     $.ajax({
         url: "http://" + window.location.host + "/api/earthquakes/query?count=" + request.count + "&start_time="+ formatDateForQuery(request.startTime)
         + "&end_time=" + formatDateForQuery(request.endTime) + "&max_magnitude=" + request.maxMag
@@ -222,22 +245,43 @@ function doRequest(request){
         success: function (data, textStatus, jqXHR) {
             epicentres = data;
             drawEarthquake(epicentres);
-            getStationMagnitudes(epicentres);
+            // getStationMagnitudes(epicentres);
+            // getArrivals(epicentres);
         }
     })
 }
 
-function getStationMagnitudes(epicentres) {
+
+
+function getStationMagnitudes(epicentre) {
     var min_magnitude = 2.5;
     var max_magnitude = 8;
-    for(var i = 0; i < epicentres.length; ++i) {
+    $.ajax({
+        url: "http://" + window.location.host + "/api/earthquakes/stationMagnitudes/query?earthquake_id=" + epicentre.id + "&min_magnitude=" + min_magnitude + "&max_magnitude=" + max_magnitude,
+        type: "GET",
+        success: function (data, textStatus, jqXHR) {
+            // console.log(data);
+            stationMagnitudes = data;
+            stationMagnitudes2 = jQuery.extend([], stationMagnitudes.slice());
+            // drawStationMagnitudes(data);
+            setInterval(appearStations, 50);
+        }
+    })
+
+}
+
+
+function getArrivals(epicentres) {
+    var phase = "";
+    for(var i = 0; i < epicentres.length; ++i){
         var earthquake = epicentres[i];
         $.ajax({
-            url: "http://" + window.location.host + "/api/earthquakes/stationMagnitudes/query?earthquake_id=" + earthquake.id + "&min_magnitude=" + min_magnitude + "&max_magnitude=" + max_magnitude,
+            url: "http://" + window.location.host + "/api/earthquakes/arrivals/query?earthquake_id=1895389&phase=" + phase,
             type: "GET",
             success: function (data, textStatus, jqXHR) {
-                console.log(data);
-                drawStationMagnitudes(data);
+                // console.log(data);
+                arrivals = data
+                drawArrivals(data);
             }
         })
     }
@@ -249,13 +293,39 @@ function drawStationMagnitudes(data) {
         var earthquake = data[i];
         var height = convertHeight(earthquake.station.elevation);
         var stationMagnitude = new Cesium.GeometryInstance({
-            geometry: geometry,
+            geometry: geometryStation,
             modelMatrix: Cesium.Matrix4.multiplyByTranslation(
                 Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(earthquake.station.longitude, earthquake.station.latitude)),
                 new Cesium.Cartesian3(0.0, 0.0, height + maxDepth), new Cesium.Matrix4()),
             id: earthquake,
             attributes: {
                 color: new Cesium.ColorGeometryInstanceAttribute(interpolateColorByMagnitude(0, earthquake), interpolateColorByMagnitude(1, earthquake), interpolateColorByMagnitude(2, earthquake))
+            }
+        });
+        // console.log(stationMagnitude);
+        primitivesArray.push(stationMagnitude);
+    }
+    var primitive = new Cesium.Primitive({
+        geometryInstances : primitivesArray,
+        appearance: new Cesium.PerInstanceColorAppearance({translucent: false})
+    });
+
+    scene.primitives.add(primitive);
+}
+
+function drawArrivals(data) {
+    var primitivesArray = [];
+    for(var i = 0; i < data.length; ++i){
+        var earthquake = data[i];
+        var height = convertHeight(earthquake.pick.station.elevation);
+        var stationMagnitude = new Cesium.GeometryInstance({
+            geometry: geometryElevation,
+            modelMatrix: Cesium.Matrix4.multiplyByTranslation(
+                Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(earthquake.pick.station.longitude, earthquake.pick.station.latitude)),
+                new Cesium.Cartesian3(0.0, 0.0, height + maxDepth), new Cesium.Matrix4()),
+            id: earthquake,
+            attributes: {
+                color: new Cesium.ColorGeometryInstanceAttribute(0, 0, 1)
             }
         });
         primitivesArray.push(stationMagnitude);
@@ -267,7 +337,6 @@ function drawStationMagnitudes(data) {
 
     scene.primitives.add(primitive);
 }
-
 function drawEarthquake(data){
     var primitivesArray = [];
     var neighborArray = [];
@@ -276,14 +345,14 @@ function drawEarthquake(data){
         var earthquake = data[i];
         var height = convertHeight(-earthquake.origin.depth);
         var epicentre = new Cesium.GeometryInstance({
-            geometry: geometry,
+            geometry: geometryElevation,
             modelMatrix: Cesium.Matrix4.multiplyByTranslation(
                 Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(earthquake.origin.longitude, earthquake.origin.latitude)),
                 new Cesium.Cartesian3(0.0, 0.0, height + maxDepth), new Cesium.Matrix4()),
             id: earthquake,
             attributes: {
                 // color: new Cesium.ColorGeometryInstanceAttribute(interpolateColorByMagnitude(0, earthquake), interpolateColorByMagnitude(1, earthquake), interpolateColorByMagnitude(2, earthquake), 0.3)
-                color: new Cesium.ColorGeometryInstanceAttribute(1, 0, 0, 0.3)
+                color: new Cesium.ColorGeometryInstanceAttribute(interpolateColorByMagnitude(0, earthquake), interpolateColorByMagnitude(1, earthquake), interpolateColorByMagnitude(2, earthquake))
 
             }
         });
@@ -312,7 +381,7 @@ function drawEpicentreNeighborood(epicentre, height, neighborArray) {
     for(var i = starting_point_x; i > -starting_point_x - unit; i-=unit){
 
         var neighbor = new Cesium.GeometryInstance({
-            geometry: geometry,
+            geometry: geometryElevation,
             modelMatrix: Cesium.Matrix4.multiplyByTranslation(
                 Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(epicentre.id.origin.longitude, epicentre.id.origin.latitude)),
                 new Cesium.Cartesian3(-i, 0, height + maxDepth), new Cesium.Matrix4()),
@@ -324,7 +393,7 @@ function drawEpicentreNeighborood(epicentre, height, neighborArray) {
         neighborArray.push(neighbor);
         for(var j = starting_point_y; j > -starting_point_y - unit; j-=unit){
             var neighbor = new Cesium.GeometryInstance({
-                geometry: geometry,
+                geometry: geometryElevation,
                 modelMatrix: Cesium.Matrix4.multiplyByTranslation(
                     Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(epicentre.id.origin.longitude, epicentre.id.origin.latitude)),
                     new Cesium.Cartesian3(-i, -j, height + maxDepth), new Cesium.Matrix4()),
@@ -336,7 +405,7 @@ function drawEpicentreNeighborood(epicentre, height, neighborArray) {
             neighborArray.push(neighbor);
             for(var z = starting_point_z; z > -starting_point_z - unit; z-=unit){
                 var neighbor = new Cesium.GeometryInstance({
-                    geometry: geometry,
+                    geometry: geometryElevation,
                     modelMatrix: Cesium.Matrix4.multiplyByTranslation(
                         Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(epicentre.id.origin.longitude, epicentre.id.origin.latitude)),
                         new Cesium.Cartesian3(-i,  -j, -z + height + maxDepth), new Cesium.Matrix4()),
@@ -372,34 +441,69 @@ function containsObject(obj, list) {
 }
 
 handler = new Cesium.ScreenSpaceEventHandler(viewer2.scene.canvas);
+
 handler.setInputAction(function (click) {
 
-console.log(scene.pick(click.position));
+// console.log(scene.pick(click.position));
+//     doubleClickHandler(click);
+    singleClickHandler(click);
 }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 
 handler.setInputAction(function(click) {
-    // doubleClickHandler(click);
+    doubleClickHandler(click);
 
 }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+var earthquakeDate;
+function singleClickHandler(click){
+    var pickedObject = scene.pick(click.position);
+    if(pickedObject !== undefined) {
+        var id = pickedObject.id.id;
+        for (var i = 0; i < epicentres.length; ++i) {
+            var epicentre = epicentres[i];
+            if(epicentre.id === id){
+                earthquakeDate = new Date(epicentre.origin.time);
+                viewer2.selectedEntity = new Cesium.Entity({
+                    id:  epicentre.id + "      " + epicentre.magnitude.magnitude,
+                    description: formatDateForList(earthquakeDate)
+                });
+                getStationMagnitudes(epicentre);
+            }
+        }
+        for (var i = 0; i < stationMagnitudes2.length; ++i) {
+            var stationMagnitude = stationMagnitudes2[i];
+            if(stationMagnitude.id === id){
+                var date = new Date(stationMagnitude.amplitude.time);
+                date.setHours(earthquakeDate.getHours());
+                viewer2.selectedEntity = new Cesium.Entity({
+                    id:  stationMagnitude.station.name + "      " + stationMagnitude.magnitude,
+                    description: formatDateForList(date)
+                });
+            }
+        }
 
-const zoomFactor = 1.3;
+    }
+}
+
+const zoomFactor = 0.8;
 
 function doubleClickHandler(click){
+    var cameraHeight = 2661459.364219676;
     var pickedObject = scene.pick(click.position);
-    var cameraHeight = scene.camera.positionCartographic.height;
     if(pickedObject !== undefined) {
-        var id = pickedObject.id;
+        var id = pickedObject.id.id;
         for (var i = 0; i < elevations.length; i++) {
             var e = elevations[i];
             if (e.id === id) {
-                console.log(Cesium.Ellipsoid.WGS84.cartesianToCartographic(Cesium.Matrix4.getTranslation(pickedObject.primitive.modelMatrix, new Cesium.Cartesian3())));
-                var cartographicPosition = Cesium.Ellipsoid.WGS84.cartesianToCartographic(Cesium.Matrix4.getTranslation(pickedObject.primitive.modelMatrix, new Cesium.Cartesian3()));
-                var pointHeight = cartographicPosition.height;
+                var pointHeight = pickedObject.id.elevation + maxDepth;
                 cameraHeight -= (cameraHeight/zoomFactor);
+                console.log(cameraHeight + "before");
                 if(cameraHeight < pointHeight + 100){
+                    console.log("in");
                     cameraHeight = pointHeight + 100;
                 }
+
                 moveCameraTo(e, cameraHeight);
+                console.log(cameraHeight + "after");
                 return;
             }
         }
@@ -415,7 +519,37 @@ function moveCameraTo(e, height){
             height),
         orientation: {
             heading: 0.0,
-            pitch: -Cesium.Math.PI_OVER_TWO,
-            roll: 0.0
+            pitch: Cesium.Math.toRadians(-10),
+            roll: viewer2.camera.roll
         }});
+}
+
+
+
+
+function appearStations() {
+    var primitivesArray = [];
+    if(stationMagnitudes.length > 0) {
+        var earthquake = stationMagnitudes.shift();
+
+        var height = convertHeight(earthquake.station.elevation);
+        var stationMagnitude = new Cesium.GeometryInstance({
+            geometry: geometryStation,
+            modelMatrix: Cesium.Matrix4.multiplyByTranslation(
+                Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(earthquake.station.longitude, earthquake.station.latitude)),
+                new Cesium.Cartesian3(0.0, 0.0, height + maxDepth), new Cesium.Matrix4()),
+            id: earthquake,
+            attributes: {
+                color: new Cesium.ColorGeometryInstanceAttribute(interpolateColorByMagnitude(0, earthquake), interpolateColorByMagnitude(1, earthquake), interpolateColorByMagnitude(2, earthquake), 0.1)
+            }
+        });
+        primitivesArray.push(stationMagnitude);
+
+        var primitive = new Cesium.Primitive({
+            geometryInstances: primitivesArray,
+            appearance: new Cesium.PerInstanceColorAppearance({translucent: true})
+        });
+
+        scene.primitives.add(primitive);
+    }
 }
